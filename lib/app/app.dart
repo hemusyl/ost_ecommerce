3829d09b6729b658a9a26164fd0770dac3d3e332
hemusyl <humayun1278@gmail.com>
2025-08-29 12:37:37 -0400

    Added firebase analytics and crashlytics

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,23 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 
-class CraftyBay extends StatelessWidget {
+class CraftyBay extends StatefulWidget {
   const CraftyBay({super.key});
 
   @override
+  State<CraftyBay> createState() => _CraftyBayState();
+}
+
+class _CraftyBayState extends State<CraftyBay> {
+
+  //Analytics Setup
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(
+    analytics: analytics,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(navigatorObservers: [observer],);
   }
 }
