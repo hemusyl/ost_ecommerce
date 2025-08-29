fd32e7f2a33ea126001581683f417842cdbb07b4
hemusyl <humayun1278@gmail.com>
2025-08-29 11:45:15 -0400

    Updated

---
+++ android/app/src/main/kotlin/com/wpdevusa/ost_ecommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.wpdevusa.ost_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:ost_ecommerce/firebase_options.dart';
+import 'app/app.dart';
+
+// localization
+// theme
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(const CraftyBay());
+}
