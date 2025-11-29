d258ac676eda6f41bc3fbb376fb3d57c1520e53e
hemusyl <humayun1278@gmail.com>
2025-11-29 14:10:40 +0600

    Added countdown timer for OTP

--- lib/features/auth/presentation/screens/verity_otp.dart
+++ lib/features/auth/presentation/screens/verity_otp.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:ost_ecommerce/features/auth/presentation/controllers/verify_otp_controller.dart';
 import 'package:ost_ecommerce/features/auth/presentation/screens/sign_in_screen.dart';
 import 'package:ost_ecommerce/features/shared/presentation/widgets/centered_circular_progress.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 
+import '../../../../app/app_colors.dart';
 import '../../../../app/controllers/auth_controller.dart';
 import '../../../shared/presentation/screens/bottom_nav_holder_screen.dart';
 import '../../../shared/presentation/widgets/snack_bar_message.dart';
 import '../../data/models/verify_otp_request_model.dart';
 import '../widgets/app_logo.dart';
+
+
+class OtpController extends GetxController {
+  var secondsRemaining = 120.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _startTimer();
+  }
+
+  void _startTimer() async {
+    while (secondsRemaining.value > 0) {
+      await Future.delayed(const Duration(seconds: 1));
+      secondsRemaining.value--;
+    }
+  }
+}
+
 
 class VerifyOtpScreen extends StatefulWidget {
   const VerifyOtpScreen({super.key, required this.email});
@@ -29,6 +48,7 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final OtpController controller = Get.put(OtpController());
 
     return Scaffold(
       body: SafeArea(
@@ -70,6 +90,35 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
                   }
                 ),
                 const SizedBox(height: 16),
+
+                // Timer with GetX
+                Obx(() {
+                  if (controller.secondsRemaining.value > 0) {
+                    return Text(
+                      "This code will expire in ${controller.secondsRemaining.value}s",
+                      style: textTheme.bodyMedium,
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                }),
+
+                const SizedBox(height: 8),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.secondsRemaining.value == 0) {
+                      controller.secondsRemaining.value = 120;
+                      controller.onInit();
+                    }
+                  },
+                  child: Text(
+                    "Resend Code",
+                    style: textTheme.bodyMedium?.copyWith(
+                      color: AppColors.themeColor,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
                 TextButton(
                   onPressed: _onTapBackToLoginButton,
                   child: Text('Back to Login'),
